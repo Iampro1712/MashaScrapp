f36cc58de9a92dda3648dfaa0f723db030c4141f
ThevenRexOff <thevenrexoficial@oulook.es>
2024-08-23 19:39:21 -0500

    Delete: Remove Dead Code

--- config/config.py
+++
@@ -1,4 +0,0 @@
-API_ID = 18693077
-API_HASH = "c6c4c78e3590bd62551a2e3c51ff7040"
-BOT_TOKEN = "7110370975:AAGUH9UtinscZmyUscDT1vKQr6QzILjhgJY"
-URI_DB = "mongodb+srv://cpagoyt:lkfm7SnWEJfZGbo2@masha.42zec.mongodb.net/?retryWrites=true&w=majority&appName=masha"
