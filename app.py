88169dbc078ef4a2c5c6938825605d7a4fd9706c
ThevenRexOff <thevenrexoficial@outlook.es>
2024-09-08 19:53:11 -0500

    Fix: Plugin standard

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import logging
-from pyrogram import filters
-from src.custom import custom_keywords
 
 load_dotenv()  # take environment variables from .env.
 
@@ -18,25 +16,13 @@ try:
         api_id=int(os.environ['API_ID']),
         api_hash=os.environ['API_HASH'],
         phone_number=os.environ['PHONE_NUMBER'],
-        parse_mode=ParseMode.MARKDOWN
+        parse_mode=ParseMode.MARKDOWN,
+        plugins=dict(
+            root="plugins"
+        )
     )
 
-    @app.on_message(filters.chat())
-    @app.on_edited_message()
-    async def collector_message(client, message):
-        try:
-            upper_text = message.text.upper()
-        except AttributeError:
-            log.info("Message not text: %s", message.text)
-            return
-        
-        if any(keyword in upper_text for keyword in custom_keywords) == True:
-            log.info("Found message with keywork: %s", message.text)
-            with open('messages.txt', 'a') as f:
-                f.write(f'{message.text}\n')
-
-        log.info("Found message: %s", message.text)
-
+    log.info("Running...")
     app.run()
 except KeyboardInterrupt:
     log.info("Shutting Down...")
