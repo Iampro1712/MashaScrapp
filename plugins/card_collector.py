b5ed63a763f4e0e6ebb8a0f22b99a1770afbd01c
ThevenRexOff <thevenrexoficial@outlook.es>
2024-09-10 17:01:28 -0500

    Feat: Card Collector by keywords

--- plugins/card_collector.py
+++ plugins/card_collector.py
@@ -1,22 +1,23 @@
 import logging
 from pyrogram import Client
-from pyrogram import filters
-from src.custom import custom_keywords
+from src.parser import Parser
+from src.card import parseData
 
 log = logging.getLogger('app')
 
 @Client.on_message()
 @Client.on_edited_message()
 async def collector_message(client, message):
-    try:
-        upper_text = message.text.upper()
-    except AttributeError:
-        log.info("Message not text: %s", message.text)
-        return
         
-    if any(keyword in upper_text for keyword in custom_keywords) == True:
-        log.info("Card found: %s", message.text)
-        with open('messages.txt', 'a') as f:
-            f.write(f'{message.text}\n')
+    parser = Parser(message.text)
+    card_parser = parseData(message.text)
+
+    if parser.find_keyword() and card_parser['success']:
+        log.info("Card found: %s", card_parser)
+        log.info("Keyword: %s", parser.formated_keyword())
+        # TODO: create database by using mongodb
+        with open('lives.txt', 'a') as f:
+            del card_parser['success']
+            f.write(f'{"|".join(card_parser.values())}|{parser.formated_keyword()}\n')
 
     log.info("Found message: %s", message.text)
