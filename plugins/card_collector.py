0081b245320ffb0b47001563c308c367844eb307
ThevenRexOff <thevenrexoficial@outlook.es>
2024-09-16 18:17:29 -0500

    Fix: Async Sending

--- plugins/card_collector.py
+++ plugins/card_collector.py
@@ -16,7 +16,7 @@ async def collector_message(client: Client, message):
     if parser.find_keyword() and card_parser['success']:
         log.info("Card found: %s", card_parser)
         log.info("Keyword: %s", parser.formated_keyword())
-        client.send_message(os.getenv('CHANNEL_ID'), f'Card found: {card_parser}\nResult: {parser.formated_keyword()}')
+        await client.send_message(os.getenv('CHANNEL_ID'), f'Card found: {card_parser}\nResult: {parser.formated_keyword()}')
         with open('lives.txt', 'a') as f:
             del card_parser['success']
             f.write(f'{"|".join(card_parser.values())}|{parser.formated_keyword()}\n')
