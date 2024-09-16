5265a30a6cebe69dd17ddfcb27c02c3567fc6643
ThevenRexOff <thevenrexoficial@outlook.es>
2024-09-16 17:54:01 -0500

    Fix: Not brain user

--- plugins/card_collector.py
+++ plugins/card_collector.py
@@ -1,12 +1,10 @@
+import os
 import logging
 from pyrogram import Client
 from src.parser import Parser
 from src.card import parseData
-from src.sender import send
 
 log = logging.getLogger('app')
-
-msg_fr = None
 
 @Client.on_message()
 @Client.on_edited_message()
@@ -19,10 +17,7 @@ async def collector_message(client: Client, message):
     if parser.find_keyword() and card_parser['success']:
         log.info("Card found: %s", card_parser)
         log.info("Keyword: %s", parser.formated_keyword())
+        client.send_message(os.getenv('CHANNEL_ID'), f'Card found: {card_parser}\nResult: {parser.formated_keyword()}')
         with open('lives.txt', 'a') as f:
             del card_parser['success']
             f.write(f'{"|".join(card_parser.values())}|{parser.formated_keyword()}\n')
-
-    log.info("\nFound message: %s", message.text)
-    msg_fr = message.text
-    await send(client, msg_fr)
